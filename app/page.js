3807e084807fb2daa1b7a74fd58918c7b7eccd15
domieee <gartzdominik@gmail.com>
2023-08-13 21:30:32 +0200

    ASD

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import { Typography } from '@mui/joy'
 import Navigation from './components/Navigation'
 import Image from 'next/image'
 
+import styles from './home.module.css'
+
 export default function Home() {
   return (
     <>
@@ -13,23 +15,21 @@ export default function Home() {
           alt="Picture of the author"
         />
       </div>
-      <section>
-
+      <section className={styles.landingPage}>
         <article>
           <Typography component='h1' level='h1'>Empowering Duelists with In-Depth Yu-Gi-Oh! Tournament Insights</Typography>
           <Typography component='p' level='body-md'>Elevate your dueling prowess with comprehensive Yu-Gi-Oh! tournament insights. Discover winning strategies, top decks, and evolving metas. Step into the competitive arena armed with knowledge. Welcome to your dueling advantage.</Typography>
-
         </article>
       </section>
 
-      <section>
+      <section className={styles.landingPage}>
         <article>
           <Typography component='h2' level='h2'>Already curious what comes next? Watch Upcoming Tournaments</Typography>
           <Typography component='p' level='body-md'>Stay ahead of the game by keeping an eye on the exciting tournaments on the horizon. Prepare your decks and strategies for these upcoming challenges.</Typography>
         </article>
       </section>
 
-      <section>
+      <section className={styles.landingPage}>
         <article>
           <Typography component='h2' level='h2'>Have you heard the latest? Watch Recent Tournaments</Typography>
           <Typography component='p' level='body-md'>Relive the epic duels and remarkable moments from past tournaments. Explore the strategies that led to victory and the decks that dominated the competition. Learn from the history of dueling excellence and use it to shape your future triumphs.</Typography>
