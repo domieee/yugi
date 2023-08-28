7725c54f22e562d856ecee7b2ca3b44c05d5bf06
domieee <gartzdominik@gmail.com>
2023-08-28 10:17:43 +0200

    ASDA

--- app/page.js
+++ app/page.js
@@ -72,29 +72,31 @@ export default async function Home() {
     <>
       <OuterWindowWrapper>
         <section className={styles.landingPage}>
-          <Sheet>
+          <Sheet className={styles.headerContainer}>
             <Typography component='h1' level='h1'><span style={{ background: 'linear-gradient(-30deg, var(--joy-palette-primary-700), var(--joy-palette-primary-400))', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}> Empowering Duelists</span> with In-Depth Yu-Gi-Oh! Tournament Insights</Typography>
             <Typography component='p' level='body-md'>Elevate your dueling prowess with comprehensive Yu-Gi-Oh! tournament insights with Duelist Meta. Discover winning strategies, top decks, and evolving metas. Step into the competitive arena armed with knowledge. Welcome to your dueling advantage.</Typography>
-          </Sheet>
-          <div className={styles.linkRow}>
-            <Link href='/tournaments'>
-              <div className={`${styles.lpaLink} ${styles.tournamentLink}`}>
-                <div className={styles.lpaLinkShader}>
-                  <Typography level='title-lg'>Tournaments</Typography>
-                </div>
-              </div>
-            </Link>
-            <Link href='/statistics'>
-              <div className={`${styles.lpaLink} ${styles.statisticLink}`}>
-                <div className={styles.lpaLinkShader}>
-                  <Typography level='title-lg'>Statistics</Typography>
-                </div>
-              </div>
-            </Link>
-          </div>
 
+            <div className={styles.linkRow}>
+              <Link href='/tournaments'>
+                <div className={`${styles.lpaLink} ${styles.tournamentLink}`}>
+                  <div className={styles.lpaLinkShader}>
+                    <Typography variant='h3' level='title-lg'>Tournaments</Typography>
+                  </div>
+                </div>
+              </Link>
+              <Link href='/statistics'>
+                <div className={`${styles.lpaLink} ${styles.statisticLink}`}>
+                  <div className={styles.lpaLinkShader}>
+                    <Typography variant='h3' level='title-lg'>Statistics</Typography>
+                  </div>
+                </div>
+              </Link>
+
+            </div>
+            <Divider />
+          </Sheet>
         </section>
-        <Divider />
+
         <section className={styles.landingPage}>
           <article>
             <Typography component='h2' level='h3'>Recent Tournaments</Typography>
@@ -121,6 +123,14 @@ export default async function Home() {
           </article>
           <div className={styles.upcomingTournaments}>
             <UpcomingTournaments data={upcomingTournaments} />
+            <Link href='/tournaments'>
+              <Sheet className={styles.recentWatchMoreButtonUpcoming} variant='outlined' color='primary'>
+                <div className={styles.typo}>
+                  <Typography sx={{ color: `var(--joy-palette-neutral-400, #9FA6AD)` }} level='title-md' fontWeight={600}>Watch more Tournaments</Typography>
+                  {/* <MdKeyboardArrowRight style={{ height: '25px', width: '25px', color: `var(--joy-palette-neutral-400, #9FA6AD)` }} /> */}
+                </div>
+              </Sheet>
+            </Link>
           </div>
         </section>
         <Divider />
@@ -133,8 +143,10 @@ export default async function Home() {
             <div className={styles.feedbackCtaContainer}>
               <div className={styles.feedbackShader}>
                 <Typography variant='h3' level='title-lg'>Provide a Feedback</Typography>
-                <Typography>Your role is pivotal in our exploration of Yu-Gi-Oh! tournament statistics. Your experiences will mold our platform. As a user, you grasp your needs best. Your input fuels our mission to deliver impactful data to the Yu-Gi-Oh! community.</Typography>
-                <Typography level='title-sm'>Join us - your insights shape our journey.</Typography>
+                <div className={styles.textContainer}>
+                  <Typography className={styles.feedbackSubtext}>Your role is pivotal in our exploration of Yu-Gi-Oh! tournament statistics. Your experiences will mold our platform. As a user, you grasp your needs best. Your input fuels our mission to deliver impactful data to the Yu-Gi-Oh! community.</Typography>
+                  <Typography className={styles.feedbackSubtext} level='title-sm'>Join us - your insights shape our journey.</Typography>
+                </div>
               </div>
             </div>
           </Link>
